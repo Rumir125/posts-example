1ce4653ce744ec69563483030bb0150a5bb13e17
Amir Leto <amirus95@hotmail.com>
2024-01-19 17:29:03 +0100

    update modal story

--- stories/Modal.stories.tsx
+++ stories/Modal.stories.tsx
@@ -28,55 +28,55 @@ export const BasicModal: Story = {
     popupProps: { style: { borderRadius: "10px" } },
   },
   render: (args) => {
-    const [modalOpen, setModalOpen] = React.useState(true);
-
-    React.useEffect(() => {
-      if (modalOpen) {
-        return;
-      }
-      setTimeout(() => {
-        setModalOpen(true);
-      }, 1000);
-    }, [modalOpen]);
+    const [modalOpen, setModalOpen] = React.useState(false);
 
     return (
-      <Modal {...args} open={modalOpen} onClose={() => setModalOpen(false)}>
-        <div
-          style={{
-            width: "100%",
-            height: "100%",
-            borderRadius: "10px",
-            display: "flex",
-            flexDirection: "column",
-            padding: "16px",
-            boxSizing: "border-box",
-            rowGap: "8px",
-          }}
-        >
-          <div>
-            <h2 style={{ textAlign: "center" }}>Title</h2>
-          </div>
+      <div>
+        <div>
+          <Button variant="primary" onClick={() => setModalOpen(true)}>
+            Open Modal
+          </Button>
+        </div>
+        <Modal {...args} open={modalOpen} onClose={() => setModalOpen(false)}>
           <div
             style={{
-              flex: 1,
-              border: "1px solid grey",
-              borderRadius: "8px",
-              padding: "12px",
-              color: "grey",
+              width: "100%",
+              height: "100%",
+              borderRadius: "10px",
+              display: "flex",
+              flexDirection: "column",
+              padding: "16px",
+              boxSizing: "border-box",
+              rowGap: "8px",
             }}
           >
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam officia vel fugit doloremque! Sunt eaque ad veniam
-              optio quibusdam sapiente assumenda quis enim quidem eligendi
-              accusamus nemo iste, consequatur corporis.
-            </p>
+            <div>
+              <h2 style={{ textAlign: "center" }}>Title</h2>
+            </div>
+            <div
+              style={{
+                flex: 1,
+                border: "1px solid grey",
+                borderRadius: "8px",
+                padding: "12px",
+                color: "grey",
+              }}
+            >
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam officia vel fugit doloremque! Sunt eaque ad veniam
+                optio quibusdam sapiente assumenda quis enim quidem eligendi
+                accusamus nemo iste, consequatur corporis.
+              </p>
+            </div>
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button variant="primary" onClick={() => setModalOpen(false)}>
+                Apply
+              </Button>
+            </div>
           </div>
-          <div style={{ display: "flex", justifyContent: "flex-end" }}>
-            <Button variant="primary">Apply</Button>
-          </div>
-        </div>
-      </Modal>
+        </Modal>
+      </div>
     );
   },
 };
